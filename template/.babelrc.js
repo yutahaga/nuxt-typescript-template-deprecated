9b34adbb75070dbd3f938ab042c2e05b20ec2152
Yuta Haga <yutahaga7@gmail.com>
2018-07-16 15:07:12 +0900

    :up: Update dependencies

--- template/.babelrc.js
+++ template/.babelrc.js
@@ -4,6 +4,6 @@ module.exports = {
     '@babel/plugin-syntax-logical-assignment-operators',
     '@babel/plugin-syntax-nullish-coalescing-operator',
     '@babel/plugin-syntax-optional-chaining',
-    '@babel/plugin-syntax-pipeline-operator',
+    ['@babel/plugin-syntax-pipeline-operator', { proposal: 'minimal' }],
   ],
 };
