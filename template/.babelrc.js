77cc5ace91c166b993c357a356666dbf83ccdb76
Yuta Haga <yutahaga7@gmail.com>
2018-08-09 17:31:57 +0900

    :shower: Remove .babelrc.js

--- template/.babelrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@vue/app'],
-  plugins: [
-    '@babel/plugin-syntax-logical-assignment-operators',
-    '@babel/plugin-syntax-nullish-coalescing-operator',
-    '@babel/plugin-syntax-optional-chaining',
-    ['@babel/plugin-syntax-pipeline-operator', { proposal: 'minimal' }],
-  ],
-};
