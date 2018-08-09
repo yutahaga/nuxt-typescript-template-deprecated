b521eb786dc074dd5fcd5a699990be3f93447797
Yuta Haga <yutahaga7@gmail.com>
2018-08-09 17:17:32 +0900

    :shower: remove babel proposals

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   presets: ['@vue/app'],
-  plugins: [
-    '@babel/plugin-syntax-logical-assignment-operators',
-    '@babel/plugin-syntax-nullish-coalescing-operator',
-    '@babel/plugin-syntax-optional-chaining',
-    '@babel/plugin-syntax-pipeline-operator',
-  ],
 };
